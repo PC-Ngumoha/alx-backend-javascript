661c62fe42271e0fbbf1a58f09823ff7aad8c5ef
PC-Ngumoha <pcngumoha@gmail.com>
2023-09-26 20:04:46 +0100

    Update: correction attempt for task #2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  })).catch(() => new Error()).finally(() => {
+  promise.then(
+    () => ({
+      status: 200,
+      body: 'success',
+    }),
+    () => new Error()
+  ).finally(() => {
     console.log('Got a response from the API');
   });
 }
