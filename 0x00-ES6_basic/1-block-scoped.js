165cfee6e6db9dc9bce0d681a82e317dbcbe5cbf
PC-Ngumoha <pcngumoha@gmail.com>
2023-09-26 13:06:20 +0100

    Update: correction for task #1 (ES6 basics)

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
-
-    if (task || task2) {
-      task = !task;
-      task2 = !task2;
-    }
+     // eslint-disable-next-line no-unused-vars
+    const task = true;
+     // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
