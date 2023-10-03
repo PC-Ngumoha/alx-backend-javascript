9941180a2138246c43bc94afef2ecb5532674f7d
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-03 08:31:05 +0100

    Update: correction for Advanced task #100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) >= 5) {
+  let count = weakMap.get(endpoint) || 0;
+
+  count += 1;
+  weakMap.set(endpoint, count);
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-
-  if (weakMap.get(endpoint) === undefined) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
-  }
+  return count;
 }
