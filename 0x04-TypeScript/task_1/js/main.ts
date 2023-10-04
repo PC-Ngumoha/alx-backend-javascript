7b7921eda90d3ef64c7c8bd961b593f2421444e5
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-04 16:28:56 +0100

    Update: created 'printTeacher' function and  'printTeacherFunction' interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,25 +7,16 @@ interface Teacher {
   [key: string]: any,
 }
 
-// const teacher3: Teacher = {
-//   firstName: 'John',
-//   fullTimeEmployee: false,
-//   lastName: 'Doe',
-//   location: 'London',
-//   contract: false,
-// };
-
-// console.log(teacher3);
-
 interface Directors extends Teacher {
   numberOfReports: number
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+interface printTeacherFunction {
+  printTeacher: (first: string, last: string) => string
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+console.log(printTeacher('Chukwuemeka', 'Ngumoha'));
