3b3a232d913dca9b492f81811a90496bdaf6d345
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-06 18:26:37 +0100

    Update: create task_4 interfaces and merged multifile namespace 'Subjects'

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,19 @@
+/// <reference path="Teacher.ts"/>
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number
+  }
+
+  class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingReact === 0) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,19 @@
+/// <reference path="Teacher.ts"/>
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number
+  }
+
+  class React extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingReact === 0) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
