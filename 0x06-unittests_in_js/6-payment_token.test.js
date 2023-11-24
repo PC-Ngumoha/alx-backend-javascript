f822ef16b9fb711443433d743a6ed388de45eeab
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-24 14:03:09 +0100

    Update: Learned how to skip tests by creating the '7-skip.test.js' file

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,7 +1,6 @@
-const chai = require('chai');
+const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-const expect = chai.expect;
 
 describe('getPaymentTokenFromAPI', function() {
   it('Should evaluate the return value of async function', function(done) {
